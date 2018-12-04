2a499b823e69ba8fc1c0a843cc99d4c534bbafaf
Arnaud <Arnaud@Piriou>
2018-12-04 14:39:08 +0900

    [RealSenseRecorder.py] - Building RealSense pipeline - Setting RealSense parameters
    [Virtual_Vision.py]
    -Instantiating the RealSenseRecorder class

--- Virtual Vision/RealSenseRecorder.py
+++ Virtual Vision/RealSenseRecorder.py
@@ -1,9 +1,55 @@
 import time
+import pyrealsense2 as rs2
 
 
 class RealSenseRecorder(object):
+
+    def record(self) :
+        pass
+
+    def close(self) :
+
+        self.pipeline.stop()
     
-    def __init__(self, scanDuration = 0) :
+    def __init__(self, scanDuration=0, fps=30, visualPreset=3, laserPower=240, exposure=3200, gain=16) :
 
         self.scanDuration = scanDuration
+        self.fps = fps
+
+        try :
+
+            #Initialization
+            self.pipeline = rs2.pipeline()
+            self.config = rs2.config()
+
+            #Enable streams
+            self.config.enable_stream(rs2.stream.depth, 1280, 720, rs2.format.z16, self.fps)
+            self.config.enable_stream(rs2.stream.color, 1280, 720, rs2.format.bgr8, self.fps)
+
+            #Start streaming
+            self.profile = self.pipeline.start(self.config)
+            self.device = self.profile.get_device()
+            self.depthSensor = self.device.first_depth_sensor()
+
+
+            #Set parameters
+            """
+            0 -> Custom
+            1 -> Default
+            2 -> Hand
+            3 -> High Accuracy
+            4 -> High Density
+            5 -> Medium Density
+            """
+            self.depthSensor.set_option(rs2.option.visual_preset, visualPreset)
+            self.depthSensor.set_option(rs2.option.laser_power, laserPower)
+            self.depthSensor.set_option(rs2.option.exposure, exposure)
+            self.depthSensor.set_option(rs2.option.gain, gain)
+            #self.depthSensor.set_option(rs2.option.max_distance, 4.0)
+
+
+        except Exception as e :
+            
+            print("Unable to start the stream ...\n" + str(e))
+            exit()
 
--- Virtual Vision/Virtual_Vision.py
+++ Virtual Vision/Virtual_Vision.py
@@ -10,6 +10,8 @@ def main() :
     parser.add_argument("--nsec", action="store", nargs=1, default=0, type=int, required=False, help="Set the scan duration in seconds (0 for unlimited, press Q to quit)")
     args = parser.parse_args()
 
+    rsr = RealSenseRecorder.RealSenseRecorder()
+
 
 if __name__ == "__main__" :
     main()
