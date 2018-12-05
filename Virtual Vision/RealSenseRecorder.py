8371ada949502d31e60dc56b82b6718742bb380c
Arnaud <Arnaud@Piriou>
2018-12-05 11:43:43 +0900

    [RealSenseRecorder.py] - Add a file structure creator, in order to generate scan files - Add an option to sharpen images taken from the RealSense - Add a scanning system, saving RGD and depth frames, using a timer or an user interaction - Align RGB and depth frames together
    [Virtual_Vision.py]
    - Arguments add (workspace location, sharpening option)
    - Implementation of the RealSenseRecorder class control using arguments

--- Virtual Vision/RealSenseRecorder.py
+++ Virtual Vision/RealSenseRecorder.py
@@ -1,36 +1,149 @@
+import os
 import time
 import pyrealsense2 as rs2
-
+import keyboard
+import numpy as np
+import cv2
 
 class RealSenseRecorder(object):
 
-    def record(self) :
-        pass
+    def imageSharpener(self, blurryImage) :
+
+        #Sharpens a blurry image and returns it
+        kernel = np.array([[-1,-1,-1], [-1, 9,-1], [-1,-1,-1]])
+        return cv2.filter2D(blurryImage, -1, kernel)
+
+
+
+    def setupFolder(self) :
+
+        #Setup a new scan directory within the workspace (the name of the main directory will be the epoch time at the time of the creation)
+        self.rootDir = os.path.join(self.rootDir, ("RSCapture-" + str(int(time.time()))))
+        os.makedirs(self.rootDir)
+        os.makedirs(os.path.join(self.rootDir, "Depth"))
+        os.makedirs(os.path.join(self.rootDir, "Color"))
+
+
+
+    def scan(self) :
+
+        #Depending on the --nsec argument set by the user, this function will get, process and save the frames taken by the RealSense
+        #This function will use a timer or just wait for the user to press Q
+        #The depth images will be saved in a "Depth" directory, while the RGB frames will be saved within the "Color" directory
+        #If sharpening is enabled, a sharpened copy of each RGB frame will be saved with the "-sharp" extension
+
+        counter = 0
+
+        if self.scanDuration :
+
+            initTime = time.time()
+            
+            while time.time() - initTime < float(self.scanDuration) :
+
+                depthFrame, colorFrame = self.getFrame()
+                depthImage = np.asanyarray(depthFrame.data)
+                colorImage = np.asanyarray(colorFrame.data)
+                depthPath = os.path.join(os.path.join(self.rootDir, "Depth"), (str(counter) + ".png"))
+                colorPath = os.path.join(os.path.join(self.rootDir, "Color"), (str(counter) + ".jpg"))
+
+                cv2.imwrite(depthPath, depthImage)
+                cv2.imwrite(colorPath, colorImage)
+
+                if self.sharpening :
+
+                    sharpColorPath = os.path.join(os.path.join(self.rootDir, "Color"), (str(counter) + "-sharp.jpg"))
+                    #sharpDepthPath = os.path.join(os.path.join(self.rootDir, "Depth"), (str(counter) + "-sharp.png"))
+
+                    cv2.imwrite(sharpColorPath, self.imageSharpener(colorImage))
+                    #cv2.imwrite(sharpDepthPath, self.imageSharpener(depthImage))
+
+                counter += 1
+
+
+
+        else :
+
+            while not keyboard.is_pressed("q") :
+
+                depthFrame, colorFrame = self.getFrame()
+                depthImage = np.asanyarray(depthFrame.data)
+                colorImage = np.asanyarray(colorFrame.data)
+                depthPath = os.path.join(os.path.join(self.rootDir, "Depth"), (str(counter) + ".png"))
+                colorPath = os.path.join(os.path.join(self.rootDir, "Color"), (str(counter) + ".jpg"))
+
+                cv2.imwrite(depthPath, depthImage)
+                cv2.imwrite(colorPath, colorImage)
+
+                if self.sharpening :
+
+                    sharpColorPath = os.path.join(os.path.join(self.rootDir, "Color"), (str(counter) + "-sharp.jpg"))
+                    #sharpDepthPath = os.path.join(os.path.join(self.rootDir, "Depth"), (str(counter) + "-sharp.png"))
+
+                    cv2.imwrite(sharpColorPath, self.imageSharpener(colorImage))
+                    #cv2.imwrite(sharpDepthPath, self.imageSharpener(depthImage))
+
+                counter += 1
+
+                time.sleep(0.1)
+
+
+    def getFrame(self) :
+        
+        #Get, align and return frames obtained from the RealSense
+        frames = self.pipeline.wait_for_frames()
+        alignedFrames = self.aligner.process(frames)
+
+        depthFrame = alignedFrames.get_depth_frame()
+        colorFrame = alignedFrames.get_color_frame()
+
+        if depthFrame and colorFrame :
+
+            return depthFrame, colorFrame
+
+        return None
+
+
 
     def close(self) :
-
+        
+        #Close the pipeline
+        self.depthSensor.stop()
         self.pipeline.stop()
+        
+        print("Pipeline closed")
+
+
     
-    def __init__(self, scanDuration=0, fps=30, visualPreset=3, laserPower=240, exposure=3200, gain=16) :
+    def __init__(self, scanDuration, rootDir, fps=30, visualPreset=3, laserPower=240, exposure=3200, gain=16, sharpening=False) :
 
         self.scanDuration = scanDuration
         self.fps = fps
+        self.rootDir = rootDir
+        self.sharpening = sharpening
 
         try :
 
             #Initialization
             self.pipeline = rs2.pipeline()
             self.config = rs2.config()
-
             #Enable streams
             self.config.enable_stream(rs2.stream.depth, 1280, 720, rs2.format.z16, self.fps)
             self.config.enable_stream(rs2.stream.color, 1280, 720, rs2.format.bgr8, self.fps)
 
             #Start streaming
             self.profile = self.pipeline.start(self.config)
-            self.device = self.profile.get_device()
-            self.depthSensor = self.device.first_depth_sensor()
 
+        except Exception as e :
+            
+            print("Unable to start the stream ...\n" + str(e))
+            exit()
+
+
+
+        self.device = self.profile.get_device()
+        self.depthSensor = self.device.first_depth_sensor()
+
+        try :
 
             #Set parameters
             """
@@ -43,13 +156,18 @@ class RealSenseRecorder(object):
             """
             self.depthSensor.set_option(rs2.option.visual_preset, visualPreset)
             self.depthSensor.set_option(rs2.option.laser_power, laserPower)
-            self.depthSensor.set_option(rs2.option.exposure, exposure)
             self.depthSensor.set_option(rs2.option.gain, gain)
+            self.depthSensor.set_option(rs2.option.exposure, exposure)
             #self.depthSensor.set_option(rs2.option.max_distance, 4.0)
 
-
         except Exception as e :
-            
-            print("Unable to start the stream ...\n" + str(e))
-            exit()
+
+            print("Unable to set one parameter on the RealSense. Gonna continue to run.\n" + str(e))
+            pass
+
+        #Create an align object -> aligning depth frames to color frames
+        self.aligner = rs2.align(rs2.stream.color)
+
+
+        
 
--- Virtual Vision/Virtual_Vision.py
+++ Virtual Vision/Virtual_Vision.py
@@ -1,16 +1,29 @@
 import open3d
 import argparse
 import RealSenseRecorder
+import atexit
+import os
 
 
 def main() :
 
-    parser = argparse.ArgumentParser(description="Virtual Vision v0.0")
-    parser.add_argument("--scan", action="store", nargs=1, default=True, type=bool, required=True, help="Record a new scan of the environment : True/False")
-    parser.add_argument("--nsec", action="store", nargs=1, default=0, type=int, required=False, help="Set the scan duration in seconds (0 for unlimited, press Q to quit)")
-    args = parser.parse_args()
+    rootDir = os.path.join(os.path.join(os.path.join(os.environ["USERPROFILE"]), "Desktop"), "Workspace")   
 
-    rsr = RealSenseRecorder.RealSenseRecorder()
+    parser = argparse.ArgumentParser(description="Virtual Vision v0.0")
+    parser.add_argument("--scan", action="store", nargs=1, default=1, type=int, required=True, help="Record a new scan of the environment : 0/1")
+    parser.add_argument("--nsec", action="store", nargs=1, default=0, type=int, required=False, help="Set the scan duration in seconds (0 for unlimited, press Q to quit)")
+    parser.add_argument("--workspace", action="store", nargs=1, default=rootDir, type=str, required=False, help="Workspace directory, Desktop/Workspace by default")
+    parser.add_argument("--sharpening", action="store", nargs=1, default=0, type=int, required=False, help="Sharpen blurry images : 0/1")
+    args = parser.parse_args()
+    print(args.sharpening[0])
+
+    if args.scan[0] :
+    
+        rsr = RealSenseRecorder.RealSenseRecorder(scanDuration=args.nsec[0], fps=30, visualPreset=3, laserPower=240, exposure=3200, gain=16, rootDir=args.workspace, sharpening=bool(args.sharpening[0]))
+        rsr.setupFolder()
+        rsr.scan()
+        
+        atexit.register(rsr.close)
 
 
 if __name__ == "__main__" :
